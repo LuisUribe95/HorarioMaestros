a632348af1738dd9a6a0f5d8f0b501b334369fe7
LuisFrancisco95 <uribe_morelos@hotmail.com>
2017-11-29 15:44:48 -0600

    jlkj44

--- validad.php
+++ validad.php
@@ -9,10 +9,6 @@
 		exit();
 	}
 
-	mysql_connect('localhost','root','') or die("Error al conectar".mysql_error());
-
-	mysql_select_db('horarioescolar') or die("Serror al seleccionlar la base de datos".mysql_error());
-
 	$result = mysql_query("SELECT * FROM usuarios where usuario = '".$usuarios."' and passw = '".$pass."'");
 
 	if($row = mysql_fetch_array($result)){
